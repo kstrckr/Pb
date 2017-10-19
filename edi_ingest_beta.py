ca509c3b9b1afbddb5bf701a3a4d96f02158bb73
Gilt Groupe Studios <GGS@iMac-8.local>
2017-10-19 15:00:11 -0400

    working folder name printing sans special chars

--- edi_ingest_beta.py
+++ edi_ingest_beta.py
@@ -3,9 +3,27 @@ import re
 
 class Event:
 
-    __init__(self, gcal_string):
-        pattern = '(?P<category>\D{4}(\s\D{4})?)\s(?P<sale_id>\d{10})\s(?P<name>[\D*\s*]*)'
-        parse_gcal = re.search()
+    calendar_string = ''
+    category = ''
+    sale_id = ''
+    name = ''
+    underscore_name = ''
+    date = ''
+    date_digits = 0
+    
+
+
+    def __init__(self, gcal_string):
+        self.calendar_string = gcal_string
+        pattern = '(?P<category>\D{4}(\s\D{4})?)\s(?P<sale_id>\d{10})\s(?P<name>[\D*\s]*(?=\sPO|\sTO|\sPRO)).*(?P<date>(\d{2}/){2}\d{2})'
+        parse_gcal = re.search(pattern, self.calendar_string)
+        self.category = parse_gcal.group('category')
+        self.sale_id = parse_gcal.group('sale_id')
+        self.name = parse_gcal.group('name')
+        self.underscore_name = self.name.replace(' ', '_')
+        self.date = parse_gcal.group('date')
+        self.date_digits = self.date.replace('/', '')
+
 
     metadata_fields = {
         'CPRO:Art Director': None,
@@ -17,3 +35,7 @@ class Event:
         'PRTD:Brand': None,
         'PRTD:Product Catagory': None, 
     }
+
+    def __str__(self):
+        output = '{}_{}_{}_{}'.format(self.date_digits, self.underscore_name, self.sale_id, self.category,)
+        return output
